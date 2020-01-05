d47583af297c707e931c66de28cb20a420ae7f14
League Student <league@iMac-16.attlocal.net>
2020-01-05 15:57:00 -0800

    new level

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,73 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
+		Robot walle = new Robot();
+		Robot.setWindowColor(Color.BLACK);
+		walle.setX(30);
+		walle.setY(500);
+		walle.penDown();
+		walle.setSpeed(100);
+		walle.setPenWidth(5);
+		drawHouse(walle);
+		
+		
+		
 		
 	}
+	public void drawHouse(Robot walle) {
+		drawFlatRoof(walle, "small", Color.BLUE);
+		drawPointyRoof(walle, "medium", Color.RED);
+	}
+	public void drawFlatRoof(Robot walle, String shgt, Color c) {
+		int height = 0;
+		if(shgt.equalsIgnoreCase("small")) {
+			height = 60;
+		}
+		else if(shgt.equalsIgnoreCase("medium")) {
+			height = 120;
+		}
+		else if(shgt.equalsIgnoreCase("large")) {
+			height = 250;
+		}
+		walle.setPenColor(c);
+		walle.move(height);
+		walle.turn(90);
+		walle.move(20);
+		walle.turn(90);
+		walle.move(height);
+		walle.turn(-90);
+		walle.setPenColor(Color.GREEN);
+		walle.move(15);
+		walle.turn(-90);
+	}
+	public void drawPointyRoof(Robot walle, String shgt, Color c) {
+		int height = 0;
+		if(shgt.equalsIgnoreCase("small")) {
+			height = 60;
+		}
+		else if(shgt.equalsIgnoreCase("medium")) {
+			height = 120;
+		}
+		else if(shgt.equalsIgnoreCase("large")) {
+			height = 250;
+		}
+		walle.setPenColor(c);
+		walle.move(height);
+		walle.turn(45);
+		walle.move(20);
+		walle.turn(90);
+		walle.move(20);
+		walle.turn(45);
+		walle.move(height);
+		walle.turn(-90);
+		walle.setPenColor(Color.GREEN);
+		walle.move(15);
+		walle.turn(-90);
+	}
+	
 }
